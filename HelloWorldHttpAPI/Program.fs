5c587427df7c8fceb9e0ac71198eb4b39578d5e2
Juarez Sampaio <juarez.asf@gmail.com>
2022-06-18 17:03:59 -0300

    Add Giraffe to dotnet project

--- HelloWorldHttpAPI/Program.fs
+++ HelloWorldHttpAPI/Program.fs
@@ -1,15 +1,33 @@
 open System
 open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Hosting
 open Microsoft.Extensions.Hosting
+open Microsoft.Extensions.DependencyInjection
+open Giraffe
+
+let webApp =
+    choose [
+       route "/user"
+           >=> text "No user registered"
+       route "/routine"
+           >=> text "No routine registered"
+    ]
+
+let configureApp  (app: IApplicationBuilder) =
+    app.UseGiraffe webApp
+
+let configureServices (services: IServiceCollection) =
+    services.AddGiraffe() |> ignore
 
 [<EntryPoint>]
 let main args =
-    let builder = WebApplication.CreateBuilder(args)
-    let app = builder.Build()
-
-    app.MapGet("/", Func<string>(fun () -> "Hello World!")) |> ignore
-
-    app.Run()
-
-    0 // Exit code
-
+    Host.CreateDefaultBuilder()
+        .ConfigureWebHostDefaults(
+            fun webHostBuilder ->
+                webHostBuilder
+                    .Configure(configureApp)
+                    .ConfigureServices(configureServices)
+                    |> ignore)
+        .Build()
+        .Run()
+    0
